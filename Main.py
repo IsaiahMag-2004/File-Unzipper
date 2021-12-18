f864ff27c0d9f7cf80d3ca489634ce3d56da42bd
Isaiah Magana <maganaisaiah4@gmail.com>
2021-12-17 22:19:40 -0500

    Created the second option where you get both a table fo contents, and all the files extracted to the current working directory

--- Main.py
+++ Main.py
@@ -17,3 +17,12 @@ file_name = input("Enter the file name, with the .zip at the end of the name: ")
 with ZipFile (file_name, 'r') as zip:
     if choice == 1:
         zip.printdir()
+    elif choice == 2:
+        #Print A table of contents for the file
+        zip.printdir()
+
+        #Extracting all the files
+        print("Extracting all the files now...")
+        zip.extractall()
+        print("DONE!")
+        print("The files have been extracted to the current working directory")

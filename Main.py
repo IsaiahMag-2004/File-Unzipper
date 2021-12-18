fc4388ccf45f3c9a4eb77a94b7882fda4fe400a8
Isaiah Magana <maganaisaiah4@gmail.com>
2021-12-17 22:14:45 -0500

    Created sentenal value so that the user has to enter a propper number or be stuck in an infinate loop

--- Main.py
+++ Main.py
@@ -6,4 +6,9 @@ print("2.Print the contents of the zip file, and extract all the files")
 print("3.Print the contents of the zip file, and extract only one file")
 print("4.Print the contents of the zip file, and read the data from one file")
 choice = int(input("Which of the 4 choices are you gonna chose: "))
+#Sentenal value
+while choice != 1 or choice != 2 or choice != 3 or choice != 4:
+    print("That wasnt an option, chose again")
+    choice = int(input())
+
 file_name = input("Enter the file name, with the .zip at the end of the name: ")

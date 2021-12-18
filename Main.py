a1bbb3a36b5d0c9f269c90ab8a2ab959d79d0dbc
Isaiah Magana <maganaisaiah4@gmail.com>
2021-12-17 22:08:27 -0500

    Created the inital message the user will see

---
+++ Main.py
@@ -0,0 +1,7 @@
+from zipfile import ZipFile
+
+print("What would you like to do?")
+print("1.Print the content of zip file")
+print("2.Print the contents of the zip file, and extract all the files")
+print("3.Print the contents of the zip file, and extract only one file")
+print("4.Print the contents of the zip file, and read the data from one file")

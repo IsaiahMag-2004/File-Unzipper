49c1b9d9258e4327286de6b7966dc6109615e8ed
Isaiah Magana <Maganaisaiah4@gmail.com>
2021-12-17 22:37:33 -0500

    Commented out all other choices than one and two because they are not ready

--- Main.py
+++ Main.py
@@ -3,8 +3,8 @@ from zipfile import ZipFile
 print("What would you like to do?")
 print("1.Print the content of zip file")
 print("2.Print the contents of the zip file, and extract all the files")
-print("3.Print the contents of the zip file, and extract only one file")
-print("4.Print the contents of the zip file, and read the data from one file")
+# print("3.Print the contents of the zip file, and extract only one file")
+# print("4.Print the contents of the zip file, and read the data from one file")
 choice = int(input("Which of the 4 choices are you gonna chose: "))
 file_name = input("Enter the file name, with the .zip at the end of the name: ")
 
